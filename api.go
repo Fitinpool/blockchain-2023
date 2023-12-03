0e9e08097c8c132b7138e6bc3976a68eb13f75f2
Marcelo Muñoz <75703557+Chelosky-O@users.noreply.github.com>
2023-12-03 20:48:36 -0300

    Update api.go

--- api.go
+++ api.go
@@ -1,166 +1,169 @@
-// api.go
-package main
+	// api.go
+	package main
 
-import (
-	e "blockchain/entities"
-	"encoding/json"
-	"fmt"
-	"net/http"
-	"strconv"
+	import (
+		e "blockchain/entities"
+		"encoding/json"
+		"fmt"
+		"net/http"
+		"strconv"
 
-	"github.com/gorilla/mux"
-	"github.com/pkg/errors"
-)
+		"github.com/gorilla/mux"
+		"github.com/pkg/errors"
+	)
 
-var userdb Store
-var blockdb Store
+	var userdb Store
+	var blockdb Store
 
-func StartServer() {
-	var err error
-	userdb, err := NewStore("userdb")
-	if err != nil {
-		errors.Wrap(err, "NewStore error userdb")
-	}
-	defer userdb.Close()
+	func StartServer() {
+		var err error
+		userdb, err := NewStore("userdb")
+		if err != nil {
+			errors.Wrap(err, "NewStore error userdb")
+		}
+		defer userdb.Close()
 
-	blockdb, err := NewStore("blockchain")
-	if err != nil {
-		errors.Wrap(err, "NewStore error blockchain")
-	}
-	defer blockdb.Close()
+		blockdb, err := NewStore("blockchain")
+		if err != nil {
+			errors.Wrap(err, "NewStore error blockchain")
+		}
+		defer blockdb.Close()
 
-	router := mux.NewRouter()
+		router := mux.NewRouter()
 
-	// Definir rutas para la API
-	router.HandleFunc("/transacciones", CrearNuevaTransaccion).Methods("POST")
-	router.HandleFunc("/bloque/{index}", ConsultarPorBloque).Methods("GET")
-	router.HandleFunc("/ultima-transaccion", ConsultarUltimaTransaccion).Methods("GET")
-
-	// Iniciar el servidor en el puerto 3000
-	fmt.Println("Servidor iniciado en el puerto 3000")
-	http.ListenAndServe(":3000", router)
-}
-
-func CrearNuevaTransaccion(w http.ResponseWriter, r *http.Request) {
-	var tx e.Transaction
-	err := json.NewDecoder(r.Body).Decode(&tx)
-	if err != nil {
-		http.Error(w, "Error al decodificar la transacción", http.StatusBadRequest)
-		return
+		// Definir rutas para la API
+		router.HandleFunc("/transacciones", func(w http.ResponseWriter, r *http.Request) {
+			CrearNuevaTransaccion(w, r, blockdb, userdb)
+		}).Methods("POST")
+		router.HandleFunc("/bloque/{index}", func(w http.ResponseWriter, r *http.Request) {
+			BuscarBloqueHandler(w, r, blockdb)
+		}).Methods("GET")
+		router.HandleFunc("/buscar_ultima_transaccion", func(w http.ResponseWriter, r *http.Request) {
+			BuscarUltimaTransaccionHandler(w, r, blockdb)
+		}).Methods("GET")
+		// Iniciar el servidor en el puerto 3000
+		fmt.Println("Servidor iniciado en el puerto 3000")
+		http.ListenAndServe(":3000", router)
 	}
 
-	// Obtener el usuario actual desde la base de datos
-	retrievedData, err := userdb.Get(tx.Sender)
-	if err != nil {
-		http.Error(w, "Error al obtener el usuario de la base de datos", http.StatusInternalServerError)
-		return
+	func CrearNuevaTransaccion(w http.ResponseWriter, r *http.Request, blockdb *Store, userdb *Store) {
+		var params struct {
+			Remitente    string
+			Destinatario string
+			Monto        float64
+		}
+		err := json.NewDecoder(r.Body).Decode(&params)
+		if err != nil {
+			http.Error(w, "Error decodificando parámetros", http.StatusBadRequest)
+			return
+		}
+
+		// Imprimir los parámetros
+		fmt.Println("Nueva transacción:")
+		fmt.Println("- Remitente:", params.Remitente)
+		fmt.Println("- Destinatario:", params.Destinatario)
+		fmt.Println("- Monto:", params.Monto)
+
+		tx := &e.Transaction{
+			Sender:    params.Remitente,
+			Recipient: params.Destinatario,
+			Amount:    params.Monto,
+			Nonce:     1,
+		}
+
+		var resultUser e.User
+		retrievedData, err := userdb.Get(params.Remitente)
+		if err != nil {
+			errors.Wrap(err, "userdb.Get error")
+		}
+
+		err = json.Unmarshal(retrievedData, &resultUser)
+		if err != nil {
+			errors.Wrap(err, "user json.Unmarshal error")
+		}
+
+		FirmaTransaccion(tx, resultUser.PrivateKey)
+		currentBlock.Transactions = append(currentBlock.Transactions, *tx)
+
+		fmt.Println("Transaccion agregada en el bloque: " + fmt.Sprintf("%d", currentBlock.Index))
+		fmt.Println("Nonce:" + fmt.Sprint(tx.Nonce))
+
+		resultUser.Nonce = resultUser.Nonce + 1
+		resultUser.AccuntBalence = resultUser.AccuntBalence - params.Monto
+
+		err = userdb.Put(params.Remitente, resultUser)
+		if err != nil {
+			errors.Wrap(err, "case 2 userdb.Put error")
+		}
+
+		recipientPut, err := userdb.Get(params.Destinatario)
+		if err != nil {
+			errors.Wrap(err, "No existe o Error")
+		}
+
+		var recipientResult e.User
+		err = json.Unmarshal(recipientPut, &recipientResult)
+		if err != nil {
+			errors.Wrap(err, "case 2 json.Unmarshal error")
+		}
+
+		recipientResult.AccuntBalence = recipientResult.AccuntBalence + params.Monto
+
+		err = userdb.Put(params.Destinatario, recipientResult)
+		if err != nil {
+			errors.Wrap(err, "case 2 userdb.Put error")
+		}
+
+		newResult, err := userdb.Get(params.Remitente)
+		if err != nil {
+			errors.Wrap(err, "userdb.Get error")
+		}
+
+		err = json.Unmarshal(newResult, &resultUser)
+		if err != nil {
+			errors.Wrap(err, "case 2 json.Unmarshal error")
+		}
+
+		// Responder con éxito
+		w.WriteHeader(http.StatusCreated)
+
 	}
 
-	var resultUser e.User
-	err = json.Unmarshal(retrievedData, &resultUser)
-	if err != nil {
-		http.Error(w, "Error al decodificar el usuario", http.StatusInternalServerError)
-		return
-	}
+	func BuscarBloqueHandler(w http.ResponseWriter, r *http.Request, blockdb *Store) {
+		params := mux.Vars(r)
+		index, err := strconv.Atoi(params["index"])
+		if err != nil {
+			http.Error(w, "El índice del bloque debe ser un número entero", http.StatusBadRequest)
+			return
+		}
 
-	// Verificar si el usuario tiene saldo suficiente
-	if tx.Amount > resultUser.AccuntBalence {
-		http.Error(w, "No tienes saldo suficiente", http.StatusForbidden)
-		return
-	}
-
-	// Firmar la transacción
-	err = FirmaTransaccion(&tx, resultUser.PrivateKey)
-	if err != nil {
-		http.Error(w, "Error al firmar la transacción", http.StatusInternalServerError)
-		return
-	}
-
-	// Agregar la transacción al bloque actual
-	currentBlock.Transactions = append(currentBlock.Transactions, tx)
-
-	// Actualizar el nonce y el saldo del usuario
-	resultUser.Nonce++
-	resultUser.AccuntBalence -= tx.Amount
-
-	err = userdb.Put(tx.Sender, resultUser)
-	if err != nil {
-		http.Error(w, "Error al actualizar el usuario en la base de datos", http.StatusInternalServerError)
-		return
-	}
-
-	// Actualizar el saldo del destinatario
-	recipientData, err := userdb.Get(tx.Recipient)
-	if err != nil {
-		http.Error(w, "Error al obtener el destinatario de la base de datos", http.StatusInternalServerError)
-		return
-	}
-
-	var recipientResult e.User
-	err = json.Unmarshal(recipientData, &recipientResult)
-	if err != nil {
-		http.Error(w, "Error al decodificar el destinatario", http.StatusInternalServerError)
-		return
-	}
-
-	recipientResult.AccuntBalence += tx.Amount
-
-	err = userdb.Put(tx.Recipient, recipientResult)
-	if err != nil {
-		http.Error(w, "Error al actualizar el destinatario en la base de datos", http.StatusInternalServerError)
-		return
-	}
-
-	// Guardar el bloque actualizado en la base de datos
-	key := fmt.Sprintf("%05d", currentBlock.Index)
-	err = blockdb.Put(key, currentBlock)
-	if err != nil {
-		http.Error(w, "Error al guardar el bloque en la base de datos", http.StatusInternalServerError)
-		return
-	}
-
-	// Responder con el resultado de la transacción
-	w.WriteHeader(http.StatusCreated)
-	json.NewEncoder(w).Encode(tx)
-}
-
-func ConsultarPorBloque(w http.ResponseWriter, r *http.Request) {
-	params := mux.Vars(r)
-	index, err := strconv.Atoi(params["index"])
-	if err != nil {
-		http.Error(w, "Error al convertir el índice del bloque", http.StatusBadRequest)
-		return
-	}
-
-	// Obtener el bloque correspondiente al índice
-	var retrievedData []byte
-	if index == currentBlock.Index {
-		retrievedData, err = json.Marshal(currentBlock)
-	} else {
 		key := fmt.Sprintf("%05d", index)
-		retrievedData, err = blockdb.Get(key)
+
+		retrievedData, err := blockdb.Get(key)
+		if err != nil {
+			http.Error(w, "Error al buscar el bloque", http.StatusInternalServerError)
+		}
+
+		var resultBlock e.Block
+		err = json.Unmarshal(retrievedData, &resultBlock)
+		if err != nil {
+			http.Error(w, "Error al decodificar el bloque", http.StatusInternalServerError)
+			return
+		}
+
+		// Devolver el bloque como respuesta
+		w.Header().Set("Content-Type", "application/json")
+		json.NewEncoder(w).Encode(resultBlock)
 	}
 
-	if err != nil {
-		http.Error(w, "Error al obtener el bloque de la base de datos", http.StatusInternalServerError)
-		return
+	func BuscarUltimaTransaccionHandler(w http.ResponseWriter, r *http.Request, blockdb *Store) {
+		lastValue := blockdb.GetLastKey()
+		if lastValue == nil {
+			http.Error(w, "No hay transacciones", http.StatusNotFound)
+			return
+		}
+
+		// Obtener la última transacción y devolverla como respuesta
+		w.Header().Set("Content-Type", "application/json")
+		w.Write(lastValue)
 	}
-
-	// Enviar el bloque como respuesta
-	w.WriteHeader(http.StatusOK)
-	w.Write(retrievedData)
-}
-
-func ConsultarUltimaTransaccion(w http.ResponseWriter, r *http.Request) {
-	// Obtener la última transacción del bloque actual
-	if len(currentBlock.Transactions) == 0 {
-		http.Error(w, "No hay transacciones en el bloque actual", http.StatusNotFound)
-		return
-	}
-
-	lastTransaction := currentBlock.Transactions[len(currentBlock.Transactions)-1]
-
-	// Enviar la última transacción como respuesta
-	w.WriteHeader(http.StatusOK)
-	json.NewEncoder(w).Encode(lastTransaction)
-}
